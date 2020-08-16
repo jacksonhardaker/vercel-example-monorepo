815c0ab727dbbe1b3fef25b28b706d6e6c64df51
Jackson Hardaker <jackson.hardaker@gmail.com>
2020-08-15 20:26:23 -0400

    add storybook to play with storybook composition

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  refs: {
+    "design-system": { 
+      title: "Design System", 
+      url: "http://localhost:7008"
+    },
+    "component-library": { 
+      title: "Component Library", 
+      url: "http://localhost:7007"
+    }
+   }
+}
